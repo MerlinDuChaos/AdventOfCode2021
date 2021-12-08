723e47dfe052021e2382ec6a44a94d1bdb75de51
Julien Barbier <jbarbier@thelis.be>
2021-12-09 00:29:52 +0100

    Day6 - second part

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -29,6 +29,33 @@ void First()
 void Second()
 {
     var lines = File.ReadAllLines("input.txt");
+    int numberOfDays = 256;
+
+    var data = lines[0].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToList();
+
+    var dict = Enumerable.Range(0, 9).ToDictionary(i => i, i => 0L);
+
+    foreach (var group in data.GroupBy(d => d))
+    {
+        dict[group.Key] = group.Count();
+    }
+
+    for (int day = 0; day < numberOfDays; day++)
+    {
+        var newDict = Enumerable.Range(0, 9).ToDictionary(i => i, i => 0L);
+
+        for (int i = 8; i > 0; i--)
+        {
+            newDict[i - 1] = dict[i];
+        }
+
+        newDict[6] += dict[0];
+        newDict[8] += dict[0];
+
+        dict = newDict;
+    }
+
+    Console.WriteLine($"Result: {dict.Sum(d => d.Value)}");
 }
 
 Console.WriteLine("First or second half ? (1 or 2)");

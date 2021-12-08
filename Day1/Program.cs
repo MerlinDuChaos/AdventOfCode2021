557d135a59a3320718d2183053ea41df4cc7d468
Julien Barbier <jbarbier@thelis.be>
2021-12-08 14:10:21 +0100

    Added Day1

---
+++ Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Input file ?");
+var file = Console.ReadLine();
+
+if (!File.Exists(file))
+{
+    Console.WriteLine("File not found");
+    return;
+}
+
+var values = File.ReadAllLines(file).Select(x => int.Parse(x)).ToList();
+
+int cpt = 0;
+int lastValue = values.First();
+
+values.Skip(1).ToList().ForEach(x => { if (x > lastValue) { cpt++; } lastValue = x; });
+
+
+Console.WriteLine($"Result: {cpt}");

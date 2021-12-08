cdde904e59dea410f947cc4d54513783fa7104fb
Julien Barbier <jbarbier@thelis.be>
2021-12-08 14:18:56 +0100

    Added Day2

---
+++ Day2/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Input file ?");
+var file = Console.ReadLine();
+
+if (!File.Exists(file))
+{
+    Console.WriteLine("File not found");
+    return;
+}
+
+var lines = File.ReadAllLines(file);
+
+int horizontalPos = 0;
+int depth = 0;
+
+foreach (var line in lines)
+{
+    var parts = line.Split(" ");
+    var amount = int.Parse(parts[1]);
+
+    switch (parts[0])
+    {
+        case "forward":
+            horizontalPos += amount;
+            break;
+
+        case "down":
+            depth += amount;
+            break;
+
+        case "up":
+            depth -= amount;
+            break;
+    }
+}
+
+Console.WriteLine($"Result: {horizontalPos * depth}");

725218087247e617cbef779187d1ff354311c847
Julien Barbier <jbarbier@thelis.be>
2021-12-08 23:57:15 +0100

    Day4 - second part

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,9 +1,7 @@
 ﻿void First()
 {
     var lines = new Queue<string>(File.ReadAllLines("input.txt").Where(l => !string.IsNullOrWhiteSpace(l)));
-
     var draws = lines.Dequeue().Split(',').Select(i => int.Parse(i)).ToList();
-
     var boards = new List<Board>();
 
     while (lines.Any())
@@ -12,14 +10,14 @@
         boards.Add(new Board(values));
     }
 
-    Board winningBoard = null;
+    Board? winningBoard = null;
     int lastDraw = 0;
 
     foreach (int draw in draws)
     {
         lastDraw = draw;
 
-        foreach(var b in boards)
+        foreach (var b in boards)
         {
             if (b.TryCheck(draw) && b.HasBingo())
             {
@@ -41,7 +39,39 @@
 
 void Second()
 {
+    var lines = new Queue<string>(File.ReadAllLines("input.txt").Where(l => !string.IsNullOrWhiteSpace(l)));
+    var draws = lines.Dequeue().Split(',').Select(i => int.Parse(i)).ToList();
+    var boards = new List<Board>();
 
+    while (lines.Any())
+    {
+        var values = Enumerable.Range(0, 5).Select(i => lines.Dequeue()).ToArray();
+        boards.Add(new Board(values));
+    }
+
+    Board? winningBoard = null;
+    int lastDrawWhenWinningBoard = 0;
+
+    foreach (int draw in draws)
+    {
+        foreach (var b in boards.Where(board => !board.IsBingo))
+        {
+            if (b.TryCheck(draw) && b.HasBingo())
+            {
+                winningBoard = b.Clone();
+                lastDrawWhenWinningBoard = draw;
+            }
+        };
+
+        if (boards.All(b => b.IsBingo))
+        {
+            break;
+        }
+    }
+
+    var score = winningBoard?.GetScore(lastDrawWhenWinningBoard);
+
+    Console.WriteLine($"Result: {score}");
 }
 
 Console.WriteLine("First or second half ? (1 or 2)");
@@ -58,6 +88,16 @@ class Board
 
     public Cell[][] Cells { get; set; } = new Cell[5][];
     public int Id { get; }
+
+    public bool IsBingo = false;
+
+    private Board()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Cells[i] = new Cell[5];
+        }
+    }
 
     public Board(string[] lines)
     {
@@ -92,17 +132,24 @@ class Board
 
     public bool HasBingo()
     {
+        if (IsBingo)
+        {
+            return true;
+        }
+
         for (var i = 0; i < 5; i++)
         {
             if (Enumerable.Range(0, 5).Select(j => Cells[i][j].IsChecked).All(check => check))
             {
                 // BINGO horizontal
+                IsBingo = true;
                 return true;
             }
 
             if (Enumerable.Range(0, 5).Select(j => Cells[j][i].IsChecked).All(check => check))
             {
                 // Bingo horizontal
+                IsBingo = true;
                 return true;
             }
         }
@@ -116,6 +163,21 @@ class Board
                        .Sum();
 
         return sum * lastDrawnNumber;
+    }
+
+    public Board Clone()
+    {
+        var b = new Board();
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                b.Cells[i][j] = Cells[i][j];
+            }
+        }
+
+        return b;
     }
 }
 

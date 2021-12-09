f05ce6de37545afcd7f0d9bc890085d105428589
Julien Barbier <jbarbier@thelis.be>
2021-12-09 21:41:11 +0100

    Day8 - first part

---
+++ Day08/Program.cs
@@ -0,0 +1,30 @@
+﻿void First()
+{
+    var values = File.ReadAllLines("input.txt").ToList();
+
+    var lengths = new List<int> { 2, 4, 3, 7 }.ToDictionary(i => i);
+
+    var total = 0;
+
+    foreach (var value in values)
+    {
+        var output = value.Split("|")[1];
+        var outputVals = output.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        var amount = outputVals.Count(v => lengths.ContainsKey(v.Length));
+        total += amount;
+    }
+
+    Console.WriteLine($"Result: {total}");
+}
+
+void Second()
+{
+   
+}
+
+Console.WriteLine("First or second half ? (1 or 2)");
+switch (Console.ReadLine() ?? string.Empty)
+{
+    case "2": Second(); break;
+    default: First(); break;
+}
